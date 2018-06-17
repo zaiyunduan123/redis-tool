59ab8241963350acab43b4501f7d7a62352d5d03
zaiyunduan <1778667492@qq.com>
2018-06-17 22:48:18 +0800

    update

--- src/main/java/com/jesper/redis/RedisConfig.java
+++ src/main/java/com/jesper/redis/RedisConfig.java
@@ -2,7 +2,6 @@ package com.jesper.redis;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
 /**
  * 读取配置文件的redis信息
  */
